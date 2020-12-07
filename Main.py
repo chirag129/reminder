75f8960db6bec66836694e726762252f5ac5dd01
chirag129 <75439851+chirag129@users.noreply.github.com>
2020-12-07 17:35:21 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,11 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    while True: 
+      notification.notify (
+        title = "Pls Drink Water",
+        message = "The U.S. National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids a day for men. About 11.5 cups (2.7 liters) of fluids a day for women.",
+        timeout = 1
+    )
+    time.sleep (60*60)
